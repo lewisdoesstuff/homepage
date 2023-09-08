7d90baeb07b485503e8a1dce4cfc7cfd777250ba
Lewis Wood <lew@lew.ooo>
2023-09-08 19:56:26 +0100

    Added mobile page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="flex flex-col justify-center items-center w-screen h-screen bg-background text-text">
-    <div class="mt-auto flex items-center justify-between w-1/5">
-      <div class="flex flex-col justify-center items-center h-64 w-1/2 border-r border-primary">
-        <p class="text-4xl font-bold text-accent hover:text-primary transition duration-300">lew</p>
+    <div class="mt-auto flex flex-col lg:flex-row items-center justify-between w-full lg:w-1/5">
+      <div
+        class="flex flex-col justify-center items-center h-32 lg:h-64 w-1/2 border-b lg:border-b-0 lg:border-r border-primary"
+      >
+        <p
+          class="text-4xl font-bold text-accent hover:text-primary transition duration-300 cursor-default"
+        >
+          lew
+        </p>
       </div>
-      <div class="flex flex-col gap-2 w-1/2 pl-8">
+      <div class="flex flex-col items-center lg:items-start mt-8 lg:mt-0 gap-2 w-1/2 pl-8">
         <LinkButton
           v-for="link in links"
           :key="link.href"
--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex flex-col items-center justify-center w-full h-24 bg-secondary/25">
-    <p class="text-text/80">Made by lew, for lew</p>
+    <div class="flex">
+      <p class="text-text/80">Made for lew, by lew, with</p>
+      <a href="https://vuejs.org/" class="text-text/80 ml-1 underline underline-offset-2">Vue</a>
+    </div>
     <p class="text-text/80">© 2023 lew</p>
   </div>
 </template>
